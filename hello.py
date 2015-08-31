89e71d44e990381f99128bd011b37941679fc781
rcshadman <rcshadman@gmail.com>
2015-08-31 08:39:36 +0530

    Update hello.py
    removed redundant code

--- hello.py
+++ hello.py
@@ -1,97 +1,80 @@
 # Standard BFS Approach to travel through the link and extract images , level by level
-# How it work: -scrape method ,at each Level makes a new list of scraped urls and images 
-# 				, then it removes duplicates within the list,adds it to the Model and updates visited links.
-# 			   -scraper_url crawls through the link and scrapes unique urls and images
-#			   
-#			   
-# Duplicates are handled
+# How it work: -scrape method makes a new buffer set of scraped urls and images at each level
+# 				,then adds it to the model_image Set(Set handles duplicates) and updates visited links.
+# 			   -scraper_url crawls through the link and scrapes urls and images
 # Dependencies : lxml , requests
 
+
 from lxml import html
-import requests
-import urlparse
-import time
+import requests, urlparse,time
+from sets import Set
+#suppress ssl verification warning in log
+requests.packages.urllib3.disable_warnings()
 
 class Scraper:
 	
 	def __init__(self, url, depth):
-		self.targer_url = url 
+		self.targer_url = url
 		self.depth = depth
-		self.model_Images = []
-		self.model_Links = []
+		self.model_Images = Set([])
 		self.visited_Links = {}
 
-	# helper method to remove Duplicates ,not the best approach though
-	def removeDuplicates(self,temp):
-			return list(set(temp))
-	# 
+	#handles images and links at each depth
 	def scrape(self):
-		self.model_Links.append(self.targer_url)
+		target_urls = [self.targer_url]
 		current_depth=0
 		while(current_depth<=self.depth):
-			newListOfLinks = []
-			newImageList = []
-			done = 0;
-			
-			print "--------At Depth:%i---------" % (current_depth)
-			for url in self.model_Links:
-				
-				newLinks=[];newImages=[]
-				
+			#collects the images/links scrapped from each level
+			links_at_level = Set([]);images_at_level = Set([])
+			done=0
+			for url in target_urls:
 				if not self.visited_Links.has_key(url):
 					newImages,newLinks = self.scrape_url(url)
-				
+				links_at_level.update(newLinks)
+				images_at_level.update(newImages)
 				self.visited_Links.update({url:True})
-				newListOfLinks.extend(newLinks)
-				newImageList.extend(newImages)
-				done+=1;print "At Link:%i" % (done)
-				time.sleep(2)
-			self.model_Links = self.removeDuplicates(newListOfLinks)
-			self.model_Images.extend(newImageList)
-			self.model_Images = self.removeDuplicates(self.model_Images)
+				done+=1;print "  @Link:%i" % (done)
+				time.sleep(0)
+			target_urls = list(links_at_level)
+			self.model_Images |= images_at_level
 			current_depth+=1
-			print "Image Count :%d  ,Urls Count:%d"%(len(newImageList),len(newListOfLinks))
-			
-		print "--------------------printing images----------------------"
-		for img in self.model_Images:
-			print img
-		print "--------------------printing links----------------------"	
-		for link in self.model_Links:
-			print link	
-		
-	
+			print "@Depth: %d Scrapped Image Count :%d  ,Scrapped Url Count:%d"%(current_depth,images_at_level.__len__(),links_at_level.__len__())
+		print "Total Unique Images Scrapped:",self.model_Images.__len__()
 		return list(self.model_Images)
-	
+
+	#handles images and links in each link
 	def scrape_url(self,link):
-
 		response = requests.get(link)
-
+		#check for success status code
 		if(response.status_code != 200):
-			return ([],[])
+			return (set([]),set([]))
 		elif (response.status_code == 200):
-		    
-		    response_text = html.fromstring(response.content)
-		    
-		    #scraping urls from anchor tags, generate absolute url, remove duplicates
-		    links = response_text.xpath("//*/a/@href")
-		    linkList = [urlparse.urljoin(link,i) for i in links if any([urlparse.urljoin(link,i).startswith('http://'),urlparse.urljoin(link,i).startswith('ftp://'),urlparse.urljoin(link,i).startswith('tcp://'),urlparse.urljoin(link,i).startswith('sftp://')])]
-		    linkList = self.removeDuplicates(linkList)
-		    #scraping images from img tags, generate absolute url of images
-		    images = response_text.xpath("//img/@src")
-		    imageList = [urlparse.urljoin(link,i) for i in images if any([urlparse.urljoin(link,i).endswith('tif'),urlparse.urljoin(link,i).endswith('jpg'),urlparse.urljoin(link,i).endswith('gif'),urlparse.urljoin(link,i).endswith('bmp'),urlparse.urljoin(link,i).endswith('png')])]
-		    imageList=self.removeDuplicates(imageList)
-		    #scraping images from style attibute of divs 
-		    styles = response_text.xpath("//*/div/@style")
-		    for sub in styles:
-		     if "background:url" in sub:
-		       offset= sub.find("background:url")
-		       start = sub.find("(",offset,)
-		       end = sub.find(" ",offset+start,)
-		       relativelink=sub[start+1:end-1]
-		       absolutelink = urlparse.urljoin(link,relativelink)
-		       imageList.append(absolutelink)
-    
-		    return (imageList,linkList)
+			response_text = html.fromstring(response.content)
+			#scraping urls from anchor tags, generate absolute url
+			links = response_text.xpath("//a/@href")
+			linkList = [urlparse.urljoin(link,i) for i in links if any([urlparse.urljoin(link,i).startswith('http://'),urlparse.urljoin(link,i).startswith('ftp://'),urlparse.urljoin(link,i).startswith('tcp://'),urlparse.urljoin(link,i).startswith('sftp://')])]
+			#scraping images from img tags, generate absolute url of images
+			images = response_text.xpath("//img/@src")
+			imageList = [urlparse.urljoin(link,i) for i in images]
+			# scraping images from style attibute of all elements 
+			styleList = response_text.xpath("//@style")
+			for style in styleList:
+				if "background:url" in style:
+					relativelink = self.scrape_from_style_between(style,"background:url",")")
+					absolutelink = urlparse.urljoin(link,relativelink)
+					imageList.append(absolutelink)
+				if "background-image:url" in style:
+					relativelink = self.scrape_from_style_between(style,"background-image:url",")")
+					absolutelink = urlparse.urljoin(link,relativelink)
+					imageList.append(absolutelink)
+			return (set(imageList),set(linkList))
+
+	#helper method to scrap out links form style attributes		
+	def scrape_from_style_between(self,style,start_pattern,end_pattern):
+		start_index = style.find(start_pattern)+len(start_pattern)+1
+		end_index = style.find(end_pattern,start_index,)
+		relativelink=style[start_index:end_index]
+		return relativelink
 
 from flask import Flask, render_template, request
 
